9161bb127350cd7629dd8d7ac5a2524fa9871920
fawl_03 <razidias@gmail.com>
2023-03-09 16:32:20 -0300

    fix: change on connection.js

--- src/models/connection.js
+++ src/models/connection.js
@@ -5,9 +5,6 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  waitForConnection: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 module.exports = connection;
---
+++ src/services/validations/schema.js
@@ -0,0 +1,3 @@
+const Joi = require('joi');
+
+const idSchema = Joi.number().integer().min(1).required();
